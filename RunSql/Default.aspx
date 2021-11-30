657c74dc8f7fe7cbe9790f6a37003f902dd626f1
herdemo <35699746+herdemo@users.noreply.github.com>
2021-11-30 14:14:24 +0300

    first commit

---
+++ RunSql/Default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RunSql.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>RUN SQL</title>
+    <link rel="stylesheet" href="StyleSheet.css" />
+</head>
+<body>
+    <form id="form1" class="form" runat="server">
+        <div class="subscribe">
+        <h2 class="subscribe__title">Lets Run Your SQL</h2>
+            <div>
+                <asp:TextBox ID="TBSQL" runat="server" CssClass="form__email" PlaceHolder="Enter your sql"></asp:TextBox>
+                 <asp:Button ID="RunSQL" runat="server" class="form__button" Text="Run" OnClick="btn_RunSQL" />
+                 
+            </div>
+            <div>
+                <br />
+                <asp:TextBox ID="TBResult" TextMode="MultiLine" runat="server" CssClass="form__result"></asp:TextBox>
+            </div>
+            <div>
+                <br />
+                 <asp:TextBox ID="TBConStr" runat="server" CssClass="form__ConString"></asp:TextBox>
+            </div>
+           
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ RunSql/Default.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace RunSql
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        public string ConStr = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ConStr = ConfigurationManager.AppSettings["ConString"];
+            if (ConStr == "")
+                TBConStr.Text = "Connection String Emtpy";
+            else
+                TBConStr.Text = ConStr;
+
+        }
+
+        protected void btn_RunSQL(object sender, EventArgs e)
+        {
+            if(ConStr == "")
+            {
+                TBResult.Text = "Please check your connection string";
+            }else if(ConStr != TBConStr.Text){
+                UpdateAppSettings("ConString", TBConStr.Text);
+                ConStr = TBConStr.Text;
+            }
+
+            using(SqlConnection con = new SqlConnection(ConStr))
+            {
+                SqlCommand cmd;
+                SqlDataAdapter da;
+                DataSet ds = new DataSet();
+                string ret = "";
+
+                try
+                {
+                    con.Open();
+                }
+                catch (Exception ex)
+                {
+                    TBResult.Text = "CONNECTION ERROR PLEASE CHECK YOUR CONNECTIN STRING<br/>" + ConStr + "<br/>" + ex.ToString();
+                    return;
+                }
+
+                try
+                {
+                    using (cmd = new SqlCommand())
+                        cmd.Connection = con;
+                    da = new SqlDataAdapter(TBSQL.Text, con);
+                    da.Fill(ds);
+
+                    if(ds.Tables[0].Rows.Count == 0)
+                    {
+                        TBResult.Text = "SQL Result Not Found";
+                        con.Close();
+                        return;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < ds.Tables[0].Rows.Count; i++){
+                            foreach(var d in ds.Tables[0].Rows[i].ItemArray)
+                            {
+                                ret += d.ToString();
+                                ret += "|";
+                            }
+                            ret += "\n";
+                        }
+
+                        TBResult.Text = ret;
+                    }
+
+
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+
+            }
+
+
+        }
+
+
+
+        private void UpdateAppSettings(string key, string value)
+        {
+            Configuration configFile = null;
+            if (System.Web.HttpContext.Current != null)
+            {
+                configFile = WebConfigurationManager.OpenWebConfiguration("~");
+            }
+            else
+            {
+                configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            }
+            var settings = configFile.AppSettings.Settings;
+            if (settings[key] == null)
+            {
+                settings.Add(key, value);
+            }
+            else
+            {
+                settings[key].Value = value;
+            }
+            configFile.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+        }
+    }
+}
